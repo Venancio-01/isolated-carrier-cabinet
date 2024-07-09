c9ad49f359ad51c9081c57231890f1e67694c089
liqingshan <15054854614@163.com>
2024-07-09 11:54:00 +0800

    1

--- index.js
+++ index.js
@@ -7,12 +7,21 @@
 const Gpio = require('onoff').Gpio;
 const doorSensor = new Gpio(19, 'in', 'both', { debounceTimeout: 50 }); // 使用上拉电阻，防抖时间为 50 毫秒
 
+console.log('开始监听传感器状态变化...');
+
+
+setInterval(() => {
+  console.log('doorSensor.readSync():', doorSensor.readSync());
+}, 1000);
+
 // 处理传感器状态变化的回调函数
 doorSensor.watch((err, value) => {
   if (err) {
     console.error('发生错误:', err);
     return;
   }
+
+  console.log('🚀 - doorSensor.watch - value:', value)
 
   if (value === 1) {
     console.log('门是关的');
