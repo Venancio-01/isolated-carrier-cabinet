5e5fa5770ddb1817dfa02f9a5776548f27dbae96
liqingshan <15054854614@163.com>
2024-07-09 16:13:03 +0800

    1

--- src/lock.js
+++ src/lock.js
@@ -4,7 +4,8 @@ const eventEmitter = require('./utils/emit');
 const doorSensor = new Gpio(7, 'in', 'both', { debounceTimeout: 200 });
 // 已经触发过的
 let triggered = false
-let timer = null
+let closeTimer = null
+let openTimer = null
 
 function watch(fn) {
   let prevValue = doorSensor.readSync();
@@ -21,18 +22,28 @@ function watch(fn) {
 function handleGPIOChange() {
   const currentValue = doorSensor.readSync();
   if (currentValue === 0) {
-    // console.log('门已打开');
-    triggered = false
-    clearTimeout(timer)
-    timer = null
-  } else if (currentValue === 1) {
-    if (triggered) return
+    if (openTimer) return
 
-    timer = setTimeout(() => {
+    openTimer = setTimeout(() => {
+      console.log('门已打开');
+      triggered = false
+      clearTimeout(closeTimer)
+      closeTimer = null
+    }, 1000)
+
+  } else if (currentValue === 1) {
+    if (openTimer) {
+      clearTimeout(openTimer)
+      openTimer = null
+    }
+
+    if (triggered || closeTimer) return
+
+    closeTimer = setTimeout(() => {
       console.log('门已关闭');
       eventEmitter.emit('startRfidReading');
-      timer = null
-    }, 1000);
+      closeTimer = null
+    }, 2000);
   }
 }
 
