fb1b4097caeb6c067e32c86ef223b925e1769a9c
liqingshan <15054854614@163.com>
2024-07-09 11:02:58 +0800

    1

--- src/lock.js
+++ src/lock.js
@@ -8,44 +8,43 @@ let triggered = false
 
 const gpio19 = new Gpio({
   pin: 19, mode: 'in', ready: () => {
-    setInterval(function() {
-      gpio19.read()
-        .then((state) => {
-          // if (prevState !== state) {
-            logger.info('pin 19 通电变化: ' + state);
-          // }
+    gpio19.cmd('gpio mode 19 up').then(() => {
+      setInterval(function() {
+        gpio19.read()
+          .then((state) => {
+            // if (prevState !== state) {
+              logger.info('pin 19 通电变化: ' + state);
+            // }
 
-          if (state === '1') {
-            if (debounceTimer || triggered) {
-              return
-            }
-            debounceTimer = setTimeout(() => {
-              logger.info('level changed');
-              debounceTimer = null;
+            // 通电 - 开门
+            // if (state === '1') {
+            //   if (debounceTimer) {
+            //     clearTimeout(debounceTimer);
+            //     debounceTimer = null;
+            //   }
+            // }
+            // // 断电 - 关门
+            // else if (state === '0') {
+            //   if (debounceTimer || triggered) {
+            //     return
+            //   }
 
-              eventEmitter.emit('startRfidReading');
-              triggered = true
-            }, 2000);
-          } else if (state === '0') {
-            if (debounceTimer) {
-              clearTimeout(debounceTimer);
-              debounceTimer = null;
-            }
+            //   debounceTimer = setTimeout(() => {
+            //     logger.info('level changed');
+            //     debounceTimer = null;
 
-            triggered = false
-          }
+            //     eventEmitter.emit('startRfidReading');
+            //     triggered = true
+            //   }, 2000);
 
-          prevState = state;
-        });
-    }, 100)
+            //   triggered = false
+            // }
+
+            // prevState = state;
+          });
+      }, 500)
+    })
   }
 });
-
-const gpio25 = new Gpio({
-  pin: 25, mode: 'out', ready: () => {
-    gpio25.write(1);
-  }
-});
-
 
 
