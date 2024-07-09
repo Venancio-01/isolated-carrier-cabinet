015ea9ad84c78b6330c6de4f1868e1ee617d51c3
liqingshan <15054854614@163.com>
2024-07-09 15:18:05 +0800

    1

--- src/lock.js
+++ src/lock.js
@@ -1,11 +1,30 @@
 const Gpio = require('onoff').Gpio;
+const debounce = require('lodash.debounce');
 const doorSensor = new Gpio(7, 'in', 'falling', { debounceTimeout: 100 }); // 使用上拉电阻，防抖时间为 50 毫秒
 
-setInterval(() => {
-  const currentValue = doorSensor.readSync();
-  console.log('🚀 - setInterval - currentValue:', currentValue)
-}, 500);
+function watch(fn) {
+  let prevValue = doorSensor.readSync();
+  setInterval(() => {
+    const currentValue = doorSensor.readSync();
+    if (prevValue !== currentValue) {
+      fn();
+      prevValue = currentValue;
+    }
+  }, 50);
+}
 
+
+const debouncedGPIOChange = debounce(function handleGPIOChange() {
+  const currentValue = doorSensor.readSync();
+  if (currentValue === 0) {
+    console.log('门已打开');
+  } else if (currentValue === 1) {
+    console.log('门已关闭');
+  }
+}, 200)
+
+
+watch(debouncedGPIOChange);
 
 
 function unExport() {
