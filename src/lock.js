4a43119d6c5e45366c7c3dcf5a12f72954f34fb4
liqingshan <15054854614@163.com>
2024-07-17 15:49:08 +0800

    1

--- src/lock.js
+++ src/lock.js
@@ -4,34 +4,32 @@ const eventEmitter = require('./utils/emit');
 const logger = require('./utils/logger');
 
 const port = 20
-let closeDoorDebounceTimer = null;
 let prevState = null
 let triggered = false
 
 const gpio = new Gpio({
-  pin: port, mode: 'in', ready: () => {
+  pin: port, mode: 'in', ready: async () => {
+    prevState = await gpio.read()
+    console.log('🚀 - pin:port,mode:'in',ready: - prevState:', prevState)
     setInterval(function() {
       gpio.read()
         .then((state) => {
-          if (prevState !== state) { 
+          if (prevState !== state) {
             logger.info('pin ' + port + ' 通电变化: ' + state);
           }
 
           // 开门
           if (state === '0') {
             console.log('开门');
-            // isOpen = true
-            // triggered = false
+            triggered = false
           }
           // 关门
           else if (state === '1') {
-            // closeDoorDebounceTimer = setTimeout(() => {
-              console.log('关门');
-              // closeDoorDebounceTimer = null;
+            console.log('关门');
 
-            //   eventEmitter.emit('startRfidReading');
-            //   triggered = true
-            // }, 1000);
+            if (triggered) return
+            eventEmitter.emit('startRfidReading');
+            triggered = true
           }
 
           prevState = state;
