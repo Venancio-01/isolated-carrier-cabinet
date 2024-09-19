8e8a7b8c9f9ad46eb8b546cb5aa21c439a1b3768
李慧 <994380018@qq.com>
2024-09-19 14:52:29 +0800

    1

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 require("./src/utils/logger");
-require("./src/lock");
-// import './src/screen';
-// import './src/rfid';
+// import './src/lock';
+require("./src/screen");
+require("./src/rfid");
